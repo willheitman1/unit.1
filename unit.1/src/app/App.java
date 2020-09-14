7dede38701d9c1c421581741f7eafb2f88fa1492
willheitman1 <825963@mystma.org>
2020-09-14 20:17:18 +0000

    luke

---
+++ unit.1/src/app/App.java
@@ -0,0 +1,21 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("worms");
+    System.out.println("joker");
+    System.out.println("jurassic world");
+    System.out.println("the bee movie");
+    System.out.println("killer bean");
+    System.out.println("sonic");
+    System.out.println("cars");
+    System.out.println("everybodys everything");
+    System.out.println("matirx");
+    System.out.println("jaws");
+    System.out.println("meg");
+    System.out.println("angry birds");}
+
+
+
+
+}
