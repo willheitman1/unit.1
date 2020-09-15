c958e263c16f8e2f42e6ea2883e59d96b49a351d
willheitman1 <825963@mystma.org>
2020-09-15 20:14:17 +0000

    will

--- unit.1/src/app/App.java
+++ unit.1/src/app/App.java
@@ -2,20 +2,22 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("worms");
-    System.out.println("joker");
-    System.out.println("jurassic world");
-    System.out.println("the bee movie");
-    System.out.println("killer bean");
-    System.out.println("sonic");
-    System.out.println("cars");
-    System.out.println("everybodys everything");
-    System.out.println("matirx");
-    System.out.println("jaws");
-    System.out.println("meg");
-    System.out.println("angry birds");}
+        int myFavNum = 22;
+
+        System.out.println(myFavNum);
+
+        boolean livesInAlbertville = true;
+
+        System.out.println("Do I live in Albertville");
+        System.out.println(livesInAlbertville);
+        byte HowManyTonguesDoIHave = 1;
+
+        System.out.println("HowManyTonguesDoIHave");
+        System.out.println(HowManyTonguesDoIHave);
+        short HowOldAmI = 15;
 
 
-
-
+System.out.println("How old Am I");
+        System.out.println(HowOldAmI);
+ }
 }
