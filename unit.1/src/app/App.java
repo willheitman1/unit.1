388e44b8f36d3640550a547137a239da7b52fe1f
willheitman1 <825963@mystma.org>
2020-10-19 19:28:27 +0000

    challenge4

--- unit.1/src/app/App.java
+++ unit.1/src/app/App.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int targetNum = 75;
-        boolean isFinished = false;
+        
+ Scanner input = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
+        System.out.println("please enter a number");
 
-        while(isFinished == false){
-            System.out.println("Guess a number between 0 and 100");
-            int Sguess input.nextInt();
-        }
+            int answer1 = input.nextInt();
+
+              System.out.println("please enter a number");
+
+            int answer2 = input.nextInt();
+            
+            for (int i = answer1; i < answer2; i += 2) {
+        System.out.println(i);        
+     }
+
         }
 }
