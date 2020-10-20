9e52ae55e730dce45e8d5790b11b21f969c1320a
willheitman1 <825963@mystma.org>
2020-10-20 19:51:28 +0000

    madlib

--- unit.1/src/app/App.java
+++ unit.1/src/app/App.java
@@ -3,24 +3,39 @@ package app;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception { 
-      int count = 0;
-      String color = "red";
+    public static void main(String[] args) throws Exception {        boolean livesInAlbertville = false;
+     String person1 = "kyle";
+     String verb1 = "eat";
+     String place1 = "dunkin donuts";
 
+        System.out.println("do i live in albertville?");
+        System.out.println(livesInAlbertville);
+        byte howManyFingersIHave = 10;
+     System.out.println(person1 + " " + "likes too" + " " + verb1 + " " + "at" + " " + place1);
 
-      while (count <= 719) {
-          System.out.println("the count is " + count);
-          count = count + 1;
-        
-      }
+        System.out.println("how many finger do i have?");
+        System.out.println(howManyFingersIHave);
+        char statesInUnitedstates = 'f';
+     String person2 = "ashey";
+     String verb2 = "race";
+     String place2 = "town";
+     String adjective1 = "fast";
 
-      System.out.println("count finished");
+        System.out.println("how many states are in the unitedstates?");
+        System.out.println(statesInUnitedstates);
+        short willsFavNum = 19;
+     System.out.println(person2 + " " + "likes to" + " " + verb2 + " " + "all round" + " " + place2 + " " + "because she is really" + " " + adjective1);
 
-      while (color == "red"){
-          System.out.println("the color is " + color);
-          color = "blue";
-      }
+        System.out.println("what is wills favorite number");
+        System.out.println(willsFavNum);
+        long bigNum = 32489372;
+     String food = "dog";
+     String verb3 = "jumpy";
+     String adjective2 = "yummy";
+     String dayOfWeek = "saturdays";
 
-      System.out.println("color finished");
-        }
-}
+        System.out.println("what is a big number");
+        System.out.println(bigNum);
+     System.out.println("on" + " " + dayOfWeek + " " + "i get very" + " " + verb3 + " " + "becuase we sometimes get to have" + " " + food + " " + "and it is very very" + " " + adjective2);
+    }
+} 
